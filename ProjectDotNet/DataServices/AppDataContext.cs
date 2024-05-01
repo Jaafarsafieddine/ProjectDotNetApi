f6c307a9ae025b58409fcd28203a73f7fb2c9df1
Jaafarsafieddine <202010750@ua.edu.lb>
2024-05-01 18:15:57 +0300

    Add project files.

---
+++ ProjectDotNet/DataServices/AppDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectDotNet.DataServices
+{
+    public class AppDataContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+        }   
+    }
+}
---
+++ ProjectDotNet/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectDotNet.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int phoneNumber { get; set; }
+        public int cartId { get; set; }
+
+    }
+}
